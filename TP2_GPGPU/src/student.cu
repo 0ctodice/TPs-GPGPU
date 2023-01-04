c0a27d1d0242e0860d76e3eb509bb28da709df48
thomas dumont <thomas2dumont@gmail.com>
2023-01-04 15:15:43 +0100

    ended TP2

--- TP2_GPGPU/src/student.cu
+++ TP2_GPGPU/src/student.cu
@@ -55,9 +55,56 @@ namespace IMAC
 
 	__constant__ float KERNEL[100];
 
-	__global__ texture<uchar4, 1, cudaReadModeElementType> image_source;
+	texture<uchar4, 1, cudaReadModeElementType> image_source;
 
-	__global__ texture<uchar4, 2, cudaReadModeElementType> image_source2D;
+	texture<uchar4, 2, cudaReadModeElementType> image_source2D;
+
+	__global__ void convText2DCUDA(const int imgWidth, const int imgHeight, const int matSize, uchar4 *const dev_image_output)
+	{
+		int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+		int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+
+		if (x >= imgWidth || y >= imgHeight)
+		{
+			return;
+		}
+
+		float r = 0.f;
+		float g = 0.f;
+		float b = 0.f;
+
+		int offset = matSize / 2;
+
+		for (int j = -offset; j <= offset; j++)
+		{
+			int kY = j + offset;
+			for (int i = -offset; i <= offset; i++)
+			{
+				int kX = i + offset;
+				float kernel = KERNEL[kX + kY * matSize];
+
+				int tmpX = max(0, min(imgWidth - 1, x + i));
+				int tmpY = max(0, min(imgHeight - 1, y + j));
+
+				uchar4 pix = tex2D(image_source2D, tmpX, tmpY);
+
+				r += kernel * static_cast<float>(pix.x);
+				g += kernel * static_cast<float>(pix.y);
+				b += kernel * static_cast<float>(pix.z);
+			}
+		}
+
+		r = max(0.f, min(255.f, r));
+		g = max(0.f, min(255.f, g));
+		b = max(0.f, min(255.f, b));
+
+		int id = x + y * imgWidth;
+
+		dev_image_output[id].x = r;
+		dev_image_output[id].y = g;
+		dev_image_output[id].z = b;
+		dev_image_output[id].w = 255;
+	}
 
 	__global__ void convText1DCUDA(const int imgWidth, const int imgHeight, const int matSize, uchar4 *const dev_image_output)
 	{
@@ -225,13 +272,11 @@ namespace IMAC
 		dim3 block_size = dim3((imgWidth / threads_size) + 1, (imgHeight / threads_size) + 1, 1);
 		dim3 nb_threads = dim3(threads_size, threads_size, 1);
 
-		cudaMalloc((void **)&dev_image_input, bytes);
 		cudaMalloc((void **)&dev_image_output, bytes);
 
-		// cudaMemcpy(dev_image_input, &inputImg[0], bytes, cudaMemcpyHostToDevice);
-
 		// ================================================== PARTIE NAIF
-
+		// cudaMalloc((void **)&dev_image_input, bytes);
+		// cudaMemcpy(dev_image_input, &inputImg[0], bytes, cudaMemcpyHostToDevice);
 		// float *dev_mat_input = NULL;
 		// cudaMalloc((void **)&dev_mat_input, matSize * matSize * sizeof(float));
 		// cudaMemcpy(dev_mat_input, &matConv[0], matSize * matSize * sizeof(float), cudaMemcpyHostToDevice);
@@ -240,19 +285,29 @@ namespace IMAC
 
 		// ================================================== PARTIE NOYAU CONSTANT
 
+		// cudaMalloc((void **)&dev_image_input, bytes);
+		// cudaMemcpy(dev_image_input, &inputImg[0], bytes, cudaMemcpyHostToDevice);
 		// cudaMemcpyToSymbol(KERNEL, &matConv[0], matSize * matSize * sizeof(float), 0, cudaMemcpyHostToDevice);
 		// convConstCUDA<<<block_size, nb_threads>>>(imgWidth, imgHeight, matSize, dev_image_input, dev_image_output);
 
 		// ================================================== PARTIE TEXTURE 1D
 
+		// cudaMalloc((void **)&dev_image_input, bytes);
+		// cudaMemcpy(dev_image_input, &inputImg[0], bytes, cudaMemcpyHostToDevice);
 		// cudaBindTexture(0, image_source, dev_image_input, bytes);
 		// cudaMemcpyToSymbol(KERNEL, &matConv[0], matSize * matSize * sizeof(float), 0, cudaMemcpyHostToDevice);
 		// convText1DCUDA<<<block_size, nb_threads>>>(imgWidth, imgHeight, matSize, dev_image_output);
 		// cudaUnbindTexture(image_source);
 
 		// ================================================== PARTIE TEXTURE 2D
+		size_t dev_pitch;
 
+		cudaMallocPitch(&dev_image_input, &dev_pitch, imgWidth * sizeof(uchar4), imgHeight);
+		cudaMemcpy2D(dev_image_input, dev_pitch, &inputImg[0], imgWidth * sizeof(uchar4), imgWidth * sizeof(uchar4), imgHeight, cudaMemcpyHostToDevice);
+		cudaBindTexture2D(0, image_source2D, dev_image_input, imgWidth, imgHeight, dev_pitch);
 		cudaMemcpyToSymbol(KERNEL, &matConv[0], matSize * matSize * sizeof(float), 0, cudaMemcpyHostToDevice);
+		convText2DCUDA<<<block_size, nb_threads>>>(imgWidth, imgHeight, matSize, dev_image_output);
+		cudaUnbindTexture(image_source2D);
 
 		// ==================================================
 
