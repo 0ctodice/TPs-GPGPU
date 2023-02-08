7818ad47291b1e63ab00e82533f85744d6a02927
thomas dumont <thomas2dumont@gmail.com>
2023-02-08 17:02:35 +0100

    doned exo1

--- TP3_GPGPU/src/student.cu
+++ TP3_GPGPU/src/student.cu
@@ -1,32 +1,54 @@
 /*
-* TP 3 - Réduction CUDA
-* --------------------------
-* Mémoire paratagée, synchronisation, optimisation
-*
-* File: student.cu
-* Author: Maxime MARIA
-*/
+ * TP 3 - Réduction CUDA
+ * --------------------------
+ * Mémoire paratagée, synchronisation, optimisation
+ *
+ * File: student.cu
+ * Author: Maxime MARIA
+ */
 
 #include "student.hpp"
 
 namespace IMAC
 {
 	// ==================================================== EX 1
-    __global__
-    void maxReduce_ex1(const uint *const dev_array, const uint size, uint *const dev_partialMax)
+	__global__ void maxReduce_ex1(const uint *const dev_array, const uint size, uint *const dev_partialMax)
 	{
-		/// TODO
+		extern __shared__ uint sharedMemory[];
+
+		int idx = threadIdx.x;
+		int blx = blockIdx.x;
+		int myId = blx * blockDim.x + idx;
+
+		sharedMemory[idx] = myId < size ? dev_array[myId] : 0;
+
+		__syncthreads();
+
+		for (int i = 1; i < blockDim.x; i *= 2)
+		{
+			int id = idx * 2 * i;
+			if (id < blockDim.x)
+			{
+				uint maxShared = umax(sharedMemory[id], sharedMemory[id + i]);
+				sharedMemory[id] = maxShared;
+			}
+			__syncthreads();
+		}
+		if (idx == 0)
+		{
+			dev_partialMax[blx] = sharedMemory[0];
+		}
 	}
 
 	void studentJob(const std::vector<uint> &array, const uint resCPU /* Just for comparison */, const uint nbIterations)
-    {
+	{
 		uint *dev_array = NULL;
-        const size_t bytes = array.size() * sizeof(uint);
+		const size_t bytes = array.size() * sizeof(uint);
 
 		// Allocate array on GPU
-		HANDLE_ERROR( cudaMalloc( (void**)&dev_array, bytes ) );
+		HANDLE_ERROR(cudaMalloc((void **)&dev_array, bytes));
 		// Copy data from host to device
-		HANDLE_ERROR( cudaMemcpy( dev_array, array.data(), bytes, cudaMemcpyHostToDevice ) );
+		HANDLE_ERROR(cudaMemcpy(dev_array, array.data(), bytes, cudaMemcpyHostToDevice));
 
 		std::cout << "Test with " << nbIterations << " iterations" << std::endl;
 
@@ -34,58 +56,58 @@ namespace IMAC
 		uint res1 = 0; // result
 		// Launch reduction and get timing
 		float2 timing1 = reduce<KERNEL_EX1>(nbIterations, dev_array, array.size(), res1);
-		
-        std::cout << " -> Done: ";
-        printTiming(timing1);
+
+		std::cout << " -> Done: ";
+		printTiming(timing1);
 		compare(res1, resCPU); // Compare results
 
 		std::cout << "========== Ex 2 " << std::endl;
 		uint res2 = 0; // result
 		// Launch reduction and get timing
 		float2 timing2 = reduce<KERNEL_EX2>(nbIterations, dev_array, array.size(), res2);
-		
-        std::cout << " -> Done: ";
-        printTiming(timing2);
+
+		std::cout << " -> Done: ";
+		printTiming(timing2);
 		compare(res2, resCPU);
 
 		std::cout << "========== Ex 3 " << std::endl;
 		uint res3 = 0; // result
 		// Launch reduction and get timing
 		float2 timing3 = reduce<KERNEL_EX3>(nbIterations, dev_array, array.size(), res3);
-		
-        std::cout << " -> Done: ";
-        printTiming(timing3);
+
+		std::cout << " -> Done: ";
+		printTiming(timing3);
 		compare(res3, resCPU);
 
 		std::cout << "========== Ex 4 " << std::endl;
 		uint res4 = 0; // result
 		// Launch reduction and get timing
 		float2 timing4 = reduce<KERNEL_EX4>(nbIterations, dev_array, array.size(), res4);
-		
-        std::cout << " -> Done: ";
-        printTiming(timing4);
+
+		std::cout << " -> Done: ";
+		printTiming(timing4);
 		compare(res4, resCPU);
 
 		std::cout << "========== Ex 5 " << std::endl;
 		uint res5 = 0; // result
 		// Launch reduction and get timing
 		float2 timing5 = reduce<KERNEL_EX5>(nbIterations, dev_array, array.size(), res5);
-		
-        std::cout << " -> Done: ";
-        printTiming(timing5);
+
+		std::cout << " -> Done: ";
+		printTiming(timing5);
 		compare(res5, resCPU);
 
 		// Free array on GPU
-		cudaFree( dev_array );
-    }
+		cudaFree(dev_array);
+	}
 
 	void printTiming(const float2 timing)
 	{
-		std::cout << ( timing.x < 1.f ? 1e3f * timing.x : timing.x ) << " us on device and ";
-		std::cout << ( timing.y < 1.f ? 1e3f * timing.y : timing.y ) << " us on host." << std::endl;
+		std::cout << (timing.x < 1.f ? 1e3f * timing.x : timing.x) << " us on device and ";
+		std::cout << (timing.y < 1.f ? 1e3f * timing.y : timing.y) << " us on host." << std::endl;
 	}
 
-    void compare(const uint resGPU, const uint resCPU)
+	void compare(const uint resGPU, const uint resCPU)
 	{
 		if (resGPU == resCPU)
 		{
